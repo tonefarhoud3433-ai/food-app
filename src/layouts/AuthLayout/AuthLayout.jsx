417544f299b785229d5e72895108d06d5fb25e92
tonefarhoud3433-ai <tonefarhoud3433@gmail.com>
2026-04-19 13:05:41 +0200

    third commit

--- src/layouts/AuthLayout/AuthLayout.jsx
+++ src/layouts/AuthLayout/AuthLayout.jsx
@@ -1,15 +1,19 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import logo from "../../assets/images/auth/logo.png"
 
 export default function AuthLayout() {
+
+  const location = useLocation();
+  const isRegister = location.pathname === "/register"
+
   return (
     <>
-      <div className='auth-container'>
+      <div className={`auth-container ${isRegister ? "no-bg" : ""}`}>
         <div className="container-fluid bg-overlay">
-          <div className="row vh-100 justify-content-center align-items-center">
-            <div className="col-11 col-md-8 col-lg-4 bg-white rounded rounded-2 px-5 py-5">
+          <div className="row min-vh-100 justify-content-center align-items-center">
+            <div className={`col-11 ${isRegister ? "col-md-8 col-lg-6" : "col-md-8 col-lg-4"} bg-white rounded rounded-2 px-5 py-5`}>
               <div className="logo-container text-center">
-                <img className='w-75' src={logo} alt="logo" />
+                <img className={`${isRegister ? "auth-logo-size" : "w-75"}`} src={logo} alt="logo" />
               </div>
               <Outlet/>
             </div>
--- src/modules/Authentication/Components/Register/Register.jsx
+++ src/modules/Authentication/Components/Register/Register.jsx
@@ -1,9 +1,143 @@
-import React from 'react'
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 export default function Register() {
+
+  const navigate = useNavigate();
+
+  let {register,handleSubmit,formState:{errors},getValues}=useForm();
+
+  const onSubmit=async(data)=>{
+    try {
+      const response = await axios.post("https://upskilling-egypt.com:3006/api/v1/Users/Register",data);
+      const message = response?.data?.message || "Registered successfully";
+      toast.success(message);
+      navigate("/verify-acc");
+    } catch (error) {
+      const message = error.response?.data?.message || "Something went wrong";
+      toast.error(message);
+    }
+  }
+
   return (
     <div>
-      Register
+      <div className="title my-4">
+        <h3 className='auth-title'>Register</h3>
+        <span className='auth-subtitle'>Welcome Back! Please enter your details</span>
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="row gy-2 gy-lg-3 gx-3 gx-lg-5">
+        {/* user name */}
+        <div className="col-12 col-md-6">
+            <div className="auth-input-group mb-1 mb-md-4">
+              <span className='auth-icon'>
+                <i className="fa-solid fa-mobile-screen-button"></i>
+              </span>
+              <div className="divider"></div>
+              <input {...register("userName",
+                {required:"Field is Required",
+                  maxLength:{
+                    value: 8,
+                    message:"Maximum User Name is 8 characters"
+                  }
+                }
+              )} type="text" className="form-control" aria-describedby='userNameelpBlock' placeholder="userName"/>
+            </div>
+          {errors.userName && <span>{errors.userName.message}</span>}
+          </div>
+        {/* email */}
+        <div className="col-12 col-md-6">
+            <div className="auth-input-group mb-1 mb-md-4">
+              <span className='auth-icon'>
+                <i className="fa-solid fa-mobile-screen-button"></i>
+              </span>
+              <div className="divider"></div>
+              <input {...register("email",
+                {required:"Field is Required",
+                pattern:{
+                  value:/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                  message:"Email is not valid"
+                }
+                }
+              )} type="email" className="form-control" aria-describedby='emailelpBlock' placeholder="Enter your E-mail"/>
+            </div>
+          {errors.email && <span>{errors.email.message}</span>}
+          </div>
+          {/* country */}
+        <div className="col-12 col-md-6">
+            <div className="auth-input-group mb-1 mb-md-4">
+              <span className='auth-icon'>
+                <i className="fa-solid fa-mobile-screen-button"></i>
+              </span>
+              <div className="divider"></div>
+              <input {...register("country",
+                {required:"Field is Required",
+                }
+              )} type="text" className="form-control" aria-describedby='countryelpBlock' placeholder="country "/>
+            </div>
+          {errors.country && <span>{errors.country.message}</span>}
+          </div>
+          {/* phone number */}
+        <div className="col-12 col-md-6">
+            <div className="auth-input-group mb-1 mb-md-4">
+              <span className='auth-icon'>
+                <i className="fa-solid fa-mobile-screen-button"></i>
+              </span>
+              <div className="divider"></div>
+              <input {...register("phoneNumber",
+                {required:"Field is Required",
+                }
+              )} type="text" className="form-control" aria-describedby='phoneNumberelpBlock' placeholder="phoneNumber "/>
+            </div>
+          {errors.phoneNumber && <span>{errors.phoneNumber.message}</span>}
+          </div>
+          {/* password */}
+          <div className="col-12 col-md-6">
+            <div className="auth-input-group mb-1 mb-md-4">
+          <span className='auth-icon'>
+            <i className="fa-solid fa-lock"></i>
+          </span>
+          <div className="divider"></div>
+          <input {...register("password",
+            {required:"Field is Required",
+              pattern:{
+                value:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+                message:"Min 8 chars, 1 uppercase, 1 lowercase, 1 number , 1 special character"
+              }
+            }
+          )} type="password" className="form-control" aria-describedby='passwordelpBlock' placeholder="Password"/>
+        </div>
+          {errors.password && <span>{errors.password.message}</span>}
+          </div>
+          {/* confirm password */}
+          <div className="col-12 col-md-6">
+            <div className="auth-input-group mb-1 mb-md-4">
+          <span className='auth-icon'>
+            <i className="fa-solid fa-lock"></i>
+          </span>
+          <div className="divider"></div>
+          <input {...register("confirmPassword",
+            {required:"Field is Required",
+              validate:(val) => {
+                if(getValues("password") !== val) {
+                  return "Your passwords do not match"
+                }
+              }
+            }
+          )} type="password" className="form-control" aria-describedby='passwordelpBlock' placeholder="Confirm Password"/>
+        </div>
+          {errors.confirmPassword && <span>{errors.confirmPassword.message}</span>}
+          </div>
+        </div>
+        <div className="links d-flex justify-content-end my-2 my-md-3">
+          <Link className='text-decoration-none text-success' to="/login">Login Now?</Link>
+        </div>
+        <div className="text-center">
+            <button className='btn btn-success w-75'>Register</button>
+        </div>
+      </form>
     </div>
   )
 }
